0eabeafa7bd5385a25cb63865379f947d19b0f67
desireLearner <fengsongting@gmail.com>
2022-10-05 18:12:07 +0800

    Time: 95 ms (81.42%), Space: 42.4 MB (74.13%) - LeetHub

---
+++ 1-two-sum/1-two-sum.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+  const indexObj = {}
+  for (let i = 0;i < nums.length;i++) {
+    const left = target - nums[i]
+    if (indexObj[left] !== undefined) {
+      return [indexObj[left], i]
+    } else {
+      indexObj[nums[i]] = i
+    }
+  }
+};
