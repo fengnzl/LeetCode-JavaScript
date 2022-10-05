15a621a42e3d31491a773911964af4fcffcb1378
desireLearner <fengsongting@gmail.com>
2022-10-05 23:18:31 +0800

    Time: 95 ms (65.29%), Space: 44.2 MB (66.39%) - LeetHub

--- 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.js
+++ 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.js
@@ -13,18 +13,16 @@
 var levelOrder = function(root) {
   if (root == null) return []
   const result = []
-  const stack = [[root]]
+  const stack = [root]
   while (stack.length > 0) {
-    const curLevel = stack.pop()
-    const ret = []
-    const nodeArr = []
-    curLevel.forEach(node => {
-      ret.push(node.val)
-      if (node.left) nodeArr.push(node.left)
-      if (node.right) nodeArr.push(node.right)
-    })
-    ret.length && result.push(ret)
-    nodeArr.length && stack.push(nodeArr)
+    const len = stack.length
+    result.push([])
+    for (let i = 0;i < len;i++) {
+      const node = stack.shift()
+      result[result.length - 1].push(node.val)
+      if(node.left) stack.push(node.left)
+      if (node.right) stack.push(node.right)
+    }
   }
   return result
 };
