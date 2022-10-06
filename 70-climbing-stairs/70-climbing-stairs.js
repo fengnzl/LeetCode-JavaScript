1b4bf2bdcf03cf8120ccb8a28d893a9355c4fd06
desireLearner <fengsongting@gmail.com>
2022-10-06 22:08:18 +0800

    Time: 96 ms (39.42%), Space: 42.5 MB (6.86%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function(n) {
+  if (n === 1) return 1
+  if (n === 2) return 2
+  let [p, q] = [1, 2]
+  for (let i = 3;i <= n;i++) {
+    [p, q] = [q, p + q]
+  }
+  return q
+};
