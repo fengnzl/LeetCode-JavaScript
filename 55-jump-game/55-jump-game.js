d76ed2b8adbf3d09e3a440dd34e3e5fe06a24b4e
desireLearner <fengsongting@gmail.com>
2022-10-08 21:51:26 +0800

    Time: 114 ms (69.25%), Space: 46.2 MB (77.75%) - LeetHub

--- 55-jump-game/55-jump-game.js
+++ 55-jump-game/55-jump-game.js
@@ -2,14 +2,26 @@
  * @param {number[]} nums
  * @return {boolean}
  */
+// var canJump = function(nums) {
+//   // 台阶可覆盖最大范围
+//   let maximum = nums[0]
+//   const n = nums.length
+//   if (maximum >= n - 1) return true
+//   for (let i = 1;i < n && maximum >= i;i++) {
+//     maximum = Math.max(maximum, nums[i] + i)
+//     if (maximum >= n - 1) return true
+//   }
+//   return false
+// };
+
 var canJump = function(nums) {
-  // 台阶可覆盖最大范围
-  let maximum = nums[0]
   const n = nums.length
-  if (maximum >= n - 1) return true
-  for (let i = 1;i < n && maximum >= i;i++) {
-    maximum = Math.max(maximum, nums[i] + i)
-    if (maximum >= n - 1) return true
+  //  只有一个台阶，一定可以达到
+  if (n === 1) return true
+  let cover = nums[0]
+  for (let i = 1;i <= cover;i++) {
+    cover = Math.max(cover, i + nums[i])
+    if (cover >= n - 1) return true
   }
   return false
 };
