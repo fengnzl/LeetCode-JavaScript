add6ea39499ab8e493bd3d72f2bb44d41ffffeac
desireLearner <fengsongting@gmail.com>
2022-10-10 00:09:36 +0800

    Time: 139 ms (34.29%), Space: 50.3 MB (38.29%) - LeetHub

--- 918-maximum-sum-circular-subarray/918-maximum-sum-circular-subarray.js
+++ 918-maximum-sum-circular-subarray/918-maximum-sum-circular-subarray.js
@@ -9,19 +9,36 @@ var maxSubarraySumCircular = function(nums) {
    * 如果全为负数 第二种情况为0 第一种情况值小于 0 此时直接返回第一种情况值即可
    * 综上所述 取上述两种情况的最大值
    */
+  // let max = nums[0]
+  // let min = nums[0]
+  // let prevMax = 0
+  // let prevMin = 0
+  // let sum = 0
+  // const n = nums.length
+  // for (let i = 0;i < n;i++) {
+  //   sum += nums[i]
+  //   prevMax = prevMax + nums[i] > nums[i] ? prevMax + nums[i] : nums[i]
+  //   if (prevMax > max) max = prevMax
+  //   prevMin = prevMin + nums[i] < nums[i] ? prevMin + nums[i] : nums[i]
+  //   if (prevMin < min) min = prevMin
+  // }
+  // if (max < 0) return max
+  // return sum - min > max ? sum - min : max
+  
+  const n = nums.length
+  const dp1 = Array(n).fill(0)
+  const dp2 = Array(n).fill(0)
   let max = nums[0]
   let min = nums[0]
-  let prevMax = 0
-  let prevMin = 0
-  let sum = 0
-  const n = nums.length
-  for (let i = 0;i < n;i++) {
-    sum += nums[i]
-    prevMax = prevMax + nums[i] > nums[i] ? prevMax + nums[i] : nums[i]
-    if (prevMax > max) max = prevMax
-    prevMin = prevMin + nums[i] < nums[i] ? prevMin + nums[i] : nums[i]
-    if (prevMin < min) min = prevMin
+  let total = nums[0]
+  dp1[0] = dp2[0] = nums[0]
+  for (let i = 1;i < n;i++) {
+    total += nums[i]
+    dp1[i] = Math.min(dp1[i - 1] + nums[i], nums[i])
+    if (dp1[i] < min) min = dp1[i]
+    dp2[i] = Math.max(dp2[i - 1] + nums[i], nums[i])
+    if (dp2[i] > max) max = dp2[i]
   }
   if (max < 0) return max
-  return sum - min > max ? sum - min : max
+  return total - min > max ? total - min : max
 };
