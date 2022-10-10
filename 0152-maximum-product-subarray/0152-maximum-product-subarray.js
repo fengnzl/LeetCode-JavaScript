b7019708e8dc3d599013442eefe3ed7af3ecc1a1
desireLearner <fengsongting@gmail.com>
2022-10-10 22:45:46 +0800

    Time: 107 ms (46.45%), Space: 45.3 MB (7.26%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.js
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.js
@@ -3,34 +3,37 @@
  * @return {number}
  */
 // dp
-// var maxProduct = function(nums) {
-//   const n = nums.length
-//   const dp = Array(n).fill(0)
-//   dp[0] = nums[0]
-//   let max = nums[0]
-//   for (let i = 0;i < n;i++) {
-//     const product = dp[i - 1] * nums[i] 
-//     dp[i] = product > nums[i] ? product : nums[i]
-//     if (max < dp[i]) max = dp[i]
-//   }
-//   return max
-// };
-
-// greedy
 var maxProduct = function(nums) {
   const n = nums.length
-  if (n === 1) return nums[0]
-  let max = 1
-  let min = 1
-  let result = -Infinity
-  for (let i = 0;i < n;i++) {
-    // 如果当前小于 0  则交换最大值和最小值
-    if (nums[i] < 0) {
-      [max, min] = [min, max]
-    }
-    max = Math.max(max * nums[i], nums[i])
-    min = Math.min(min * nums[i], nums[i])
-    if (max > result) result = max
+  const dpMin = Array(n).fill(0)
+  const dpMax = Array(n).fill(0)
+  dpMin[0] = dpMax[0] = nums[0]
+  let max = nums[0]
+  for (let i = 1;i < n;i++) {
+    const dpMinProduct = dpMin[i - 1] * nums[i]
+    const dpMaxProduct = dpMax[i - 1] * nums[i]
+    dpMax[i] = Math.max(dpMinProduct, nums[i], dpMaxProduct)
+    dpMin[i] = Math.min(dpMinProduct, nums[i], dpMaxProduct)
+    if (max < dpMax[i]) max = dpMax[i]
   }
-  return result
-}
+  return max
+};
+
+// greedy
+// var maxProduct = function(nums) {
+//   const n = nums.length
+//   if (n === 1) return nums[0]
+//   let max = 1
+//   let min = 1
+//   let result = -Infinity
+//   for (let i = 0;i < n;i++) {
+//     // 如果当前小于 0  则交换最大值和最小值
+//     if (nums[i] < 0) {
+//       [max, min] = [min, max]
+//     }
+//     max = Math.max(max * nums[i], nums[i])
+//     min = Math.min(min * nums[i], nums[i])
+//     if (max > result) result = max
+//   }
+//   return result
+// }
