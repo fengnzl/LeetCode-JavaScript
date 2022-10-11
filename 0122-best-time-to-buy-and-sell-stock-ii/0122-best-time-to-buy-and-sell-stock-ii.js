8cff02b91ac62c359578acd872e4ae26b5a76ca9
desireLearner <fengsongting@gmail.com>
2022-10-11 23:48:48 +0800

    Time: 99 ms (52.95%), Space: 42.6 MB (34.39%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.js
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.js
@@ -2,17 +2,30 @@
  * @param {number[]} prices
  * @return {number}
  */
+// var maxProfit = function(prices) {
+//   const n = prices.length
+//   const dp = Array.from({ length: n }, () => Array(2).fill(0))
+//   // dp[i][0] 代表当天不持有股票的最大值
+//   // dp[i][1] 代表当天持有股票的最大值
+//   dp[0][0] = 0
+//   dp[0][1] = -prices[0]
+//   for (let i = 1;i < n;i++) {
+//     dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i])
+//     dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i])
+//   }
+//   // 最后一天不持有股票的利润最大
+//   return dp[n - 1][0]
+// };
+
+// 通过 dp 优化
 var maxProfit = function(prices) {
   const n = prices.length
-  const dp = Array.from({ length: n }, () => Array(2).fill(0))
-  // dp[i][0] 代表当天不持有股票的最大值
-  // dp[i][1] 代表当天持有股票的最大值
-  dp[0][0] = 0
-  dp[0][1] = -prices[0]
+  let notHold = 0
+  let hold = -prices[0]
   for (let i = 1;i < n;i++) {
-    dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i])
-    dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i])
+    if (hold + prices[i] > notHold) notHold = hold + prices[i]
+    if (notHold - prices[i] > hold) hold = notHold - prices[i]
   }
   // 最后一天不持有股票的利润最大
-  return dp[n - 1][0]
-};
+  return notHold
+}
