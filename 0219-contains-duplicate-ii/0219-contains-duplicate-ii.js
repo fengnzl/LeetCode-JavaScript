41b8576858c0211e16dcb1d6fa847842ea41fe2b
desireLearner <fengsongting@gmail.com>
2022-10-21 20:51:13 +0800

    Time: 129 ms (89.20%), Space: 56 MB (77.23%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.js
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.js
@@ -4,13 +4,25 @@
  * @return {boolean}
  */
 var containsNearbyDuplicate = function(nums, k) {
+  // 双层循环 O(n^2)
+  // const n = nums.length
+  // for (let i = 0;i < n;i++) {
+  //   for (let j = i + 1;j < n;j++) {
+  //     if (nums[i] === nums[j] && j - i <= k) {
+  //       return true
+  //     }
+  //   }
+  // }
+  // return false
+  // 双指针
   const n = nums.length
+  const set = new Set()
   for (let i = 0;i < n;i++) {
-    for (let j = i + 1;j < n;j++) {
-      if (nums[i] === nums[j] && j - i <= k) {
-        return true
-      }
+    if (i > k) {
+      set.delete(nums[i - k - 1])
     }
+    if (set.has(nums[i])) return true
+    set.add(nums[i])
   }
   return false
 };
