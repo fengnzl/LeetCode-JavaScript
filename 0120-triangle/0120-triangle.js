6c94f6173fcda8dd92387f0108cb67aabe697fff
desireLearner <fengsongting@gmail.com>
2022-10-22 10:39:26 +0800

    Time: 99 ms (64.71%), Space: 44.4 MB (18.11%) - LeetHub

---
+++ 0120-triangle/0120-triangle.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[][]} triangle
+ * @return {number}
+ */
+var minimumTotal = function(triangle) {
+  const n = triangle.length
+  const dp = Array.from({ length: n }, () => Array(n).fill(0))
+  dp[0][0] = triangle[0][0]
+  for (let i = 1;i < n;i++) {
+    for (let j = 0; j <= i;j++) {
+      if (j === 0) {
+        dp[i][j] = dp[i - 1][j] + triangle[i][j]
+      } else if (j === i) {
+        dp[i][j] = dp[i - 1][j - 1] + triangle[i][j]
+      } else {
+        dp[i][j] = Math.min(dp[i - 1][j], dp[i - 1][j - 1]) + triangle[i][j]
+      }
+    }
+  }
+  return Math.min.apply(null, dp[n - 1])
+};
