dee8b560e5cc973ad3a39500ef1cfb81f6ca7d98
desireLearner <fengsongting@gmail.com>
2022-10-22 10:47:01 +0800

    Time: 81 ms (83.75%), Space: 41.6 MB (99.85%) - LeetHub

--- 0120-triangle/0120-triangle.js
+++ 0120-triangle/0120-triangle.js
@@ -2,20 +2,38 @@
  * @param {number[][]} triangle
  * @return {number}
  */
+// var minimumTotal = function(triangle) {
+//   const n = triangle.length
+//   const dp = Array.from({ length: n }, () => Array(n).fill(0))
+//   dp[0][0] = triangle[0][0]
+//   for (let i = 1;i < n;i++) {
+//     for (let j = 0; j <= i;j++) {
+//       if (j === 0) {
+//         dp[i][j] = dp[i - 1][j] + triangle[i][j]
+//       } else if (j === i) {
+//         dp[i][j] = dp[i - 1][j - 1] + triangle[i][j]
+//       } else {
+//         dp[i][j] = Math.min(dp[i - 1][j], dp[i - 1][j - 1]) + triangle[i][j]
+//       }
+//     }
+//   }
+//   return Math.min.apply(null, dp[n - 1])
+// };
+
 var minimumTotal = function(triangle) {
   const n = triangle.length
-  const dp = Array.from({ length: n }, () => Array(n).fill(0))
-  dp[0][0] = triangle[0][0]
+  const dp = Array(n).fill(0)
+  dp[0] = triangle[0][0]
   for (let i = 1;i < n;i++) {
-    for (let j = 0; j <= i;j++) {
-      if (j === 0) {
-        dp[i][j] = dp[i - 1][j] + triangle[i][j]
-      } else if (j === i) {
-        dp[i][j] = dp[i - 1][j - 1] + triangle[i][j]
+    for (let j = i;j >= 0;j--) {
+      if (j === i) {
+        dp[j] = dp[j - 1] + triangle[i][j]
+      } else if (j === 0) {
+        dp[j] = dp[j] + triangle[i][j]
       } else {
-        dp[i][j] = Math.min(dp[i - 1][j], dp[i - 1][j - 1]) + triangle[i][j]
+        dp[j] = Math.min(dp[j], dp[j - 1]) + triangle[i][j]
       }
     }
   }
-  return Math.min.apply(null, dp[n - 1])
-};
+  return Math.min(...dp)
+}
