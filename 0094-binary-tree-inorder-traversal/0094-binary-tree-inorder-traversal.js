fc67db4c4dc2346a8089713fc095807018d91641
desireLearner <fengsongting@gmail.com>
2022-11-10 23:30:08 +0800

    Time: 64 ms (93.01%), Space: 42.6 MB (14.02%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.js
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.js
@@ -10,14 +10,48 @@
  * @param {TreeNode} root
  * @return {number[]}
  */
-var inorderTraversal = function(root) {
-  const result = []
-  dfs(root)
-  return result
-  function dfs(root) {
-    if (root == null) return
-    dfs(root.left)
-    result.push(root.val)
-    dfs(root.right)
+// var inorderTraversal = function(root) {
+//   const result = []
+//   dfs(root)
+//   return result
+//   function dfs(root) {
+//     if (root == null) return
+//     dfs(root.left)
+//     result.push(root.val)
+//     dfs(root.right)
+//   }
+// };
+
+// status 为1表示将要扩展左子树，左子节点入栈
+// status 为2表示左子树扩展完毕，将要扩展右子树，右子节点入栈
+// status 为3表示右子树扩展完毕，出栈
+var inorderTraversal = function(root)  {
+  if (root == null) return []
+  const ans = []
+  const stack = [new StatusNode(1, root)]
+  while (stack.length) {
+    const topNode = stack[stack.length - 1]
+    const { node, status } = topNode
+    if (status === 1) {
+      topNode.status = 2
+      if (node.left) {
+        stack.push(new StatusNode(1, node.left))
+      }
+    }
+    if (status === 2) {
+      topNode.status = 3
+      ans.push(node.val)
+      if (node.right) {
+        stack.push(new StatusNode(1, node.right))
+      }
+    }
+    if (status === 3) {
+      stack.pop()
+    }
   }
-};
+  return ans
+}
+function StatusNode (status, node) {
+  this.status = status
+  this.node = node
+}
