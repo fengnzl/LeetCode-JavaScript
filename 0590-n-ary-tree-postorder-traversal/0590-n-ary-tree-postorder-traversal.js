641262290c2eb3a3dfebd71f2812b9f233a69d70
desireLearner <fengsongting@gmail.com>
2022-11-14 00:00:27 +0800

    Time: 91 ms (90.06%), Space: 45.9 MB (32.16%) - LeetHub

--- 0590-n-ary-tree-postorder-traversal/0590-n-ary-tree-postorder-traversal.js
+++ 0590-n-ary-tree-postorder-traversal/0590-n-ary-tree-postorder-traversal.js
@@ -10,15 +10,42 @@
  * @param {Node|null} root
  * @return {number[]}
  */
+// var postorder = function(root) {
+//   const ans = []
+//   postOrder(root)
+//   return ans
+//   function postOrder(root) {
+//     if (root == null) return
+//     for(let child of root.children) {
+//       postOrder(child)
+//     }
+//     ans.push(root.val)
+//   }
+// };
 var postorder = function(root) {
+  if (root == null) return []
   const ans = []
-  postOrder(root)
-  return ans
-  function postOrder(root) {
-    if (root == null) return
-    for(let child of root.children) {
-      postOrder(child)
+  const stack = [new StatusNode(1, root)]
+  while (stack.length) {
+    const topNode = stack[stack.length - 1]
+    if (topNode.status === 1) {
+      topNode.status = 2
+      if (topNode.node.children) {
+        for (let i = topNode.node.children.length - 1;i >= 0;i--) {
+          stack.push(new StatusNode(1, topNode.node.children[i]))
+        }
+      }
+      continue
     }
-    ans.push(root.val)
+    if (topNode.status === 2) {
+      ans.push(topNode.node.val)
+      stack.pop()
+    }
   }
+  return ans
 };
+
+function StatusNode(status, node) {
+  this.status = status
+  this.node = node
+}
