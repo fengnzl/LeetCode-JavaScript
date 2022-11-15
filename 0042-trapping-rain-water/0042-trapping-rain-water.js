7d0c4643bbdb98b70b5907b1ebe5b27106c5560e
desireLearner <fengsongting@gmail.com>
2022-11-15 22:50:33 +0800

    Time: 64 ms (98.44%), Space: 44.8 MB (39.40%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.js
+++ 0042-trapping-rain-water/0042-trapping-rain-water.js
@@ -4,22 +4,22 @@
  */
 var trap = function(height) {
   const n = height.length
-  if (n === 1) return 0
-  const stack = []
+  if (n <= 1) return 0
   let sum = 0
+  const stack = []
   for (let i = 0;i < n;i++) {
-    if (stack.length === 0) {
+    if (!stack.length) {
       stack.push(i)
       continue
     }
-    while (stack.length && height[stack.at(-1)] < height[i]) {
+    while (stack.length && height[i] > height[stack[stack.length - 1]]) {
       const top = stack.pop()
       if (!stack.length) {
         break
       }
-      const h = Math.min(height[i], height[stack.at(-1)]) - height[top]
-      const w = i - stack.at(-1) - 1
-      sum += h*w
+      const h = Math.min(height[i], height[stack[stack.length - 1]]) - height[top]
+      const w = i - stack[stack.length - 1] - 1
+      sum += h * w
     }
     stack.push(i)
   }
