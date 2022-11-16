b4d71a3b20e6a65a3ca098917a02fe01fe1a7773
desireLearner <fengsongting@gmail.com>
2022-11-16 21:58:13 +0800

    Time: 119 ms (54.14%), Space: 46.7 MB (45.30%) - LeetHub

---
+++ 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.js
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var findBottomLeftValue = function(root) {
+  let result
+  const stack = [root]
+  while (stack.length) {
+  const node = stack.shift()
+    result = node.val
+    if (node.right) stack.push(node.right)
+    if (node.left) stack.push(node.left)
+  }
+  return result
+};
