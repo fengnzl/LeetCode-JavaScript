9ee2c70a07ec3c2e48b53a4b9d5bb4957c21615f
desireLearner <fengsongting@gmail.com>
2022-11-17 22:22:33 +0800

    Time: 144 ms (71.90%), Space: 48 MB (66.62%) - LeetHub

---
+++ 0623-add-one-row-to-tree/0623-add-one-row-to-tree.js
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} val
+ * @param {number} depth
+ * @return {TreeNode}
+ */
+var addOneRow = function(root, val, depth) {
+  if (depth === 1) {
+    return new TreeNode(val, root)
+  }
+  dfs(root, 1)
+  return root
+  function dfs(root, stage) {
+    if (root == null) return
+    const left = root.left
+    const right = root.right
+    if (stage === depth - 1) {
+      root.left = new TreeNode(val, left)
+      root.right = new TreeNode(val, null, right)
+      return
+    }
+    dfs(left, stage + 1)
+    dfs(right, stage + 1)
+  }
+};
