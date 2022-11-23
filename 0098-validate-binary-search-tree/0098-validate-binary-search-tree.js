a0c56f0bc040c6800009996f0f1f95d92378aed4
desireLearner <fengsongting@gmail.com>
2022-11-24 00:16:50 +0800

    Time: 110 ms (63.08%), Space: 46.7 MB (22.73%) - LeetHub

---
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.js
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isValidBST = function(root) {
+  if (root == null)return true
+  return isValid(root, -Infinity, Infinity)
+  function isValid(root, left, right) {
+    if (root == null) return true
+    if (root.val <= left || root.val >= right) return false
+    return isValid(root.left, left, root.val) && isValid(root.right, root.val, right)
+  } 
+};
