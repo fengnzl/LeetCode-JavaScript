d959e6c6528d5fc45db43cc74f60c61e7eb5c0dc
desireLearner <fengsongting@gmail.com>
2022-12-07 23:00:36 +0800

    Time: 84 ms (84.38%), Space: 44 MB (96.14%) - LeetHub

---
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var oddEvenList = function(head) {
+  if (head == null) return head
+  let odd = head
+  let even = head.next
+  let initialEven = head.next
+  while (even != null && even.next != null) {
+    odd.next = odd.next.next
+    odd = odd.next
+    even.next = even.next.next
+    even = even.next
+  }
+  odd.next = initialEven
+  return head
+};
