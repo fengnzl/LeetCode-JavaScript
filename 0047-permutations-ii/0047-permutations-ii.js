cf9e306156b04437e6c9f095cf061d17b9d6d538
desireLearner <fengsongting@gmail.com>
2022-12-28 21:26:59 +0800

    Time: 90 ms (83.65%), Space: 45.2 MB (64.31%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.js
+++ 0047-permutations-ii/0047-permutations-ii.js
@@ -2,27 +2,55 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
+// var permuteUnique = function(nums) {
+//   const ans = []
+//   backtrack(nums, 0, [], new Set(), new Set())
+//   return ans
+//   function backtrack(nums, stage, path, indexSet, arrSet) {
+//     if (stage === nums.length) {
+//       const val = path.join()
+//       if (!arrSet.has(val)) {
+//         ans.push([...path])
+//         arrSet.add(val)
+//       }
+//       return
+//     }
+//     for (let i = 0;i < nums.length;i++) {
+//       if (!indexSet.has(i)) {
+//         indexSet.add(i)
+//         path.push(nums[i])
+//         backtrack(nums, stage + 1, path, indexSet, arrSet)
+//         path.pop()
+//         indexSet.delete(i)
+//       }
+//     }
+//   }
+// };
 var permuteUnique = function(nums) {
   const ans = []
-  backtrack(nums, 0, [], new Set(), new Set())
+  const map = new Map()
+  nums.forEach(val => map.set(val, (map.get(val) || 0) + 1))
+  const uniqueValue = []
+  const countArr = []
+  map.forEach((value, key) => {
+    uniqueValue.push(key)
+    countArr.push(value)
+  })
+  backtrack(uniqueValue, 0, [], nums.length)
   return ans
-  function backtrack(nums, stage, path, indexSet, arrSet) {
-    if (stage === nums.length) {
-      const val = path.join()
-      if (!arrSet.has(val)) {
-        ans.push([...path])
-        arrSet.add(val)
-      }
+  
+  function backtrack(valueArr, stage, path, len) {
+    if (stage === len) {
+      ans.push([...path])
       return
     }
-    for (let i = 0;i < nums.length;i++) {
-      if (!indexSet.has(i)) {
-        indexSet.add(i)
-        path.push(nums[i])
-        backtrack(nums, stage + 1, path, indexSet, arrSet)
-        path.pop()
-        indexSet.delete(i)
-      }
+    for (let i = 0;i < valueArr.length;i++) {
+      if (countArr[i] === 0) continue
+      path.push(valueArr[i])
+      countArr[i]--
+      backtrack(valueArr, stage + 1, path, len)
+      countArr[i]++
+      path.pop()
     }
   }
 };
