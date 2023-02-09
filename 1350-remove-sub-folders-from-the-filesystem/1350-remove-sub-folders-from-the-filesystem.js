88b8d8d069a5281395e4fffccd8d411f7a3a0947
desireLearner <fengsongting@gmail.com>
2023-02-09 22:07:40 +0800

    Time: 268 ms (37.50%), Space: 72.9 MB (5.36%) - Leecode Sync Helper

--- 1350-remove-sub-folders-from-the-filesystem/1350-remove-sub-folders-from-the-filesystem.js
+++ 1350-remove-sub-folders-from-the-filesystem/1350-remove-sub-folders-from-the-filesystem.js
@@ -3,14 +3,37 @@
  * @return {string[]}
  */
 var removeSubfolders = function(folder) {
-  folder.sort()
-  const ans = [folder[0]]
-  for (let i = 1;i < folder.length;i++) {
-    const prev = ans[ans.length - 1]
-    const len = prev.length
-    if (!(len < folder[i].length && (folder[i].slice(0, len) === prev) && folder[i][len] === '/')) {
-      ans.push(folder[i])
+  const root = new Trie()
+  for (let i = 0;i < folder.length;i++) {
+    const path = split(folder[i])
+    let cur = root
+    for (const name of path) {
+      if (!cur.children.get(name)) {
+        cur.children.set(name, new Trie())
+      }
+      cur = cur.children.get(name)
+    }
+    cur.ref = i
+  }
+  const dfs = (folder, ans, root) => {
+    if (root.ref !== -1) {
+      ans.push(folder[root.ref])
+      return
+    }
+    for (const child of root.children.values()) {
+      dfs(folder, ans, child)
     }
   }
+  const ans = []
+  dfs(folder, ans, root)
   return ans
 };
+function split(path) {
+  return path.split('/').slice(1)
+}
+class Trie {
+  constructor() {
+    this.ref = -1
+    this.children = new Map()
+  }
+}
