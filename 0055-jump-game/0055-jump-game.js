68c2714c3013f0aaa2caa7a1003df0000a51b2d2
desireLearner <fengsongting@gmail.com>
2023-03-12 22:21:00 +0800

    Time: 88 ms (21.64%), Space: 45.1 MB (33.25%) - Leecode Sync Helper

--- 0055-jump-game/0055-jump-game.js
+++ 0055-jump-game/0055-jump-game.js
@@ -5,12 +5,21 @@
 var canJump = function(nums) {
   // 对每次可达位置，计算当前位置加上数值可达的长度与记录的最大长度对比
   // 每次更新最大可达长度，判断是否可以到最后一位
-  let max = 0
-  let i = 0
+  // let max = 0
+  // let i = 0
+  // const len = nums.length
+  // for (;i < len && i <= max;i++) {
+  //   max = Math.max(max, i + nums[i])
+  //   if (max >= len ) return true
+  // }
+  // return i === len
+  // 如果只有个一个数 一定可达
   const len = nums.length
-  for (;i < len && i <= max;i++) {
+  if (len === 1) return true
+  let max = 0
+  for (let i = 0;i <= max; i++) {
     max = Math.max(max, i + nums[i])
-    if (max >= len ) return true
+    if (max >= len - 1) return true
   }
-  return i === len
+  return false
 };
