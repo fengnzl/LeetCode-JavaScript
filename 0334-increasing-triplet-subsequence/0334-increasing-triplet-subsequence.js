1135e62cdf691781de08e10d0a562b052b2a6826
desireLearner <fengsongting@gmail.com>
2023-03-19 00:10:09 +0800

    Time: 156 ms (8.24%), Space: 58.8 MB (58.24%) - Leecode Sync Helper

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.js
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.js
@@ -3,22 +3,39 @@
  * @return {boolean}
  */
 var increasingTriplet = function(nums) {
-  // 本质是最长上升子序列 数目为 3 的情况
-  // https://leetcode.cn/problems/increasing-triplet-subsequence/solutions/1205866/gong-shui-san-xie-zui-chang-shang-sheng-xa08h/
-  
-  // 这里使用 贪心
-  // first = nums[0] second = Infinity
-  // 循环当 third > second  说明有三个顺序满足 返回为 true
-  // 当first < third < second  更新 second = third
-  // 当 first > third, 则将first 更新为 third，这样 second 会在 first 前面，由于second 还是老first相对顺序保持不变 所以继续上述操作老 first 还是满足要求
-  const n = nums.length
-  if (n < 3) return false
-  let first = nums[0]
-  let second = Infinity
-  for (let i = 1;i < n;i++) {
-    if (nums[i] > second) return true
-    if (nums[i] > first) second = nums[i]
-    if (nums[i] < first) first = nums[i]
+  // 本质是计算最长递增子序列
+  // 只要计算递增子序列的长度大于 3 我们就可以返回true 否则返回false
+  const len = nums.length
+  if (len < 3) return false
+  const ans = []
+  let count = 0
+  for (let i = 0;i < nums.length;i++) {
+    // 如果是第一位 或者当前位大于结果数组的最后一位 直接添加到结果数组
+    if (count === 0 || nums[i] > ans[count - 1]) {
+      ans[count++] = nums[i]
+      if (count >= 3) return true
+    } else {
+      // 二分查找 第一位大于小于 nums[i] 的数据并进行更新
+      const j = getPos(ans, nums[i])
+      ans[j] = nums[i]
+    }
   }
   return false
 };
+
+function getPos(nums, target) {
+  let l = 0
+  let r = nums.length - 1
+  while (l <= r) {
+    const mid = l + ((r - l) >> 1)
+    if (nums[mid] > target) {
+      if (mid === 0 || nums[mid - 1] < target) {
+        return mid
+      }
+      r = mid - 1
+    } else {
+      l = mid + 1
+    }
+  }
+  return -1
+}
