d21ea6ff77e4c37983065c3db254bb4bba477bd1
desireLearner <fengsongting@gmail.com>
2023-03-28 22:35:15 +0800

    Time: 88 ms (10.35%), Space: 43.4 MB (50.42%) - Leecode Sync Helper

---
+++ 100327-li-wu-de-zui-da-jie-zhi-lcof/100327-li-wu-de-zui-da-jie-zhi-lcof.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var maxValue = function(grid) {
+  const h = grid.length
+  const w = grid[0].length
+  const dp = Array.from({ length: h }, () => Array(w).fill(0))
+  dp[0][0] = grid[0][0]
+  for (let i = 1;i < h;i++) {
+    dp[i][0] = dp[i - 1][0] + grid[i][0]
+  }
+  for (let j = 1;j < w;j++) {
+    dp[0][j] = dp[0][j - 1] + grid[0][j]
+  }
+  for (let i = 1;i < h;i++) {
+    for (let j = 1;j < w;j++) {
+      dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]) + grid[i][j]
+    }
+  }
+  return dp[h - 1][w - 1]
+};
