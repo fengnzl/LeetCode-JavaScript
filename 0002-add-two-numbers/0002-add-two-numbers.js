18c71ad7f9c0517f0da794029dfa3e55e06951b8
desireLearner <fengsongting@gmail.com>
2023-04-04 22:20:21 +0800

    Time: 136 ms (6.54%), Space: 45.9 MB (67.18%) - Leecode Sync Helper

---
+++ 0002-add-two-numbers/0002-add-two-numbers.js
@@ -0,0 +1,39 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var addTwoNumbers = function(l1, l2) {
+  if (l1 == null) return l2
+  if (l2 == null) return l1
+  let next = 0
+  let p1 = l1
+  let p2 = l2
+  const dummyHead = new ListNode()
+  let p = dummyHead
+  while (p1 !== null || p2 !== null) {
+    let sum = next
+    if (p1 !== null) {
+      sum += p1.val
+      p1 = p1.next
+    }
+    if (p2 !== null) {
+      sum += p2.val
+      p2= p2.next
+    }
+    p.next = new ListNode(sum % 10)
+    p = p.next
+    next = Math.floor(sum / 10)
+  }
+  if (next > 0) {
+    p.next = new ListNode(next)
+  }
+  return dummyHead.next
+};
