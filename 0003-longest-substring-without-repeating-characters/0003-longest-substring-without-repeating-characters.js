6799d7bf537afa84347cb518d44f11ed779cf412
desireLearner <fengsongting@gmail.com>
2023-04-05 22:47:46 +0800

    Time: 80 ms (71.41%), Space: 45.7 MB (53.06%) - Leecode Sync Helper

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -3,19 +3,17 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function(s) {
-  if (s.length === 0) return 0
-  let p = 0
+  let max = 0
+  let i = 0
   const set = new Set()
-  let max = - 1
-  for (let i = 0;i < s.length;i++) {
-    while (set.has(s[i])) {
-      set.delete(s[p])
-      p++
+  for (let j = 0;j < s.length;j++) {
+    while (set.has(s[j])) {
+      set.delete(s[i])
+      i++
     }
-    set.add(s[i])
-    const val = i - p + 1
-    if (val > max) {
-      max = val
+    set.add(s[j])
+    if (max < j - i + 1) {
+      max = j - i + 1
     }
   }
   return max
