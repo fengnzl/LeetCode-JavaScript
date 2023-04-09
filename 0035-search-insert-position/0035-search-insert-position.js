3d20e50dfc2facefe01880a850840ed5baa29193
desireLearner <fengsongting@gmail.com>
2023-04-09 12:09:21 +0800

    Time: 68 ms (21.42%), Space: 41.1 MB (66.10%) - Leecode Sync Helper

--- 0035-search-insert-position/0035-search-insert-position.js
+++ 0035-search-insert-position/0035-search-insert-position.js
@@ -3,21 +3,24 @@
  * @param {number} target
  * @return {number}
  */
-// 找到第一个大于等于 target 的位置
 var searchInsert = function(nums, target) {
   const n = nums.length
   let [low, high] = [0, n - 1]
   while (low <= high) {
     const mid = low + ((high - low) >> 1)
-    if (nums[mid] >= target) {
+    if (nums[mid] === target) {
+      return mid
+    } else if (nums[mid] > target) {
       if (mid === 0 || nums[mid - 1] < target) {
         return mid
-      } else {
-        high = mid - 1
       }
+      high = mid - 1
     } else {
+      if (mid === n - 1 || nums[mid + 1] > target) {
+        return mid + 1
+      }
       low = mid + 1
     }
   }
-  return n
+  return -1
 };
