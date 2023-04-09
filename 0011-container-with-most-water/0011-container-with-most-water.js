7fc15ced35af398622f26b302e876a466fdf32d5
desireLearner <fengsongting@gmail.com>
2023-04-09 11:14:33 +0800

    Time: 68 ms (86.83%), Space: 48.8 MB (5.48%) - Leecode Sync Helper

--- 0011-container-with-most-water/0011-container-with-most-water.js
+++ 0011-container-with-most-water/0011-container-with-most-water.js
@@ -5,19 +5,15 @@
 var maxArea = function(height) {
   // 暴力方法 双层循环  O(n^2)
   // 使用双指针，每次移动 height 小的一边
-  const n = height.length
-  let [left, right] = [0, n - 1]
-  const getCur = (left, right) => Math.min(height[left], height[right]) * (right - left)
-  let max = getCur(left, right)
+  let [left, right, max] = [0, height.length - 1, 0]
+  const getCur = (min) => height[min] * (right - left)
   while (left < right) {
     if (height[left] < height[right]) {
+      max = Math.max(max, getCur(left))
       left++
     } else {
+      max = Math.max(max, getCur(right))
       right--
-    }
-    const cur = getCur(left, right)
-    if (max < cur) {
-      max = cur
     }
   }
   return max
