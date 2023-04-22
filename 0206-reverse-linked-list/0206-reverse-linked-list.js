2c38e8375a8f672e7f7fea339e7c12a746b6e163
desireLearner <fengsongting@gmail.com>
2023-04-22 21:13:01 +0800

    Time: 64 ms (69.55%), Space: 42.9 MB (61.57%) - Leecode Sync Helper

---
+++ 0206-reverse-linked-list/0206-reverse-linked-list.js
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+  let reverse = null
+  while (head != null) {
+    const next = head.next
+    head.next = reverse
+    reverse = head
+    head = next
+  }
+  return reverse
+};
