a5a14df9fa24ab3930168da97a1edc6b985e79e2
desireLearner <fengsongting@gmail.com>
2023-04-26 00:10:56 +0800

    Time: 96 ms (92.24%), Space: 50.3 MB (74.03%) - Leecode Sync Helper

--- 2758-check-if-object-instance-of-class/2758-check-if-object-instance-of-class.js
+++ 2758-check-if-object-instance-of-class/2758-check-if-object-instance-of-class.js
@@ -7,13 +7,6 @@ var checkIfInstanceOf = function(obj, classFunction) {
   if (classFunction == null || obj == null) {
     return false
   }
-  const baseType = ['number', 'string', 'undefined', 'boolean', 'symbol', 'bigint']
-  const getType = target => Object.prototype.toString.call(target).slice(8, -1).toLowerCase()
-  const isBaseType = baseType.includes(getType(obj))
-  // () => checkIfInstance(5, Number)
-  if (isBaseType && obj === classFunction?.name) {
-    return true
-  }
   let proto = Object.getPrototypeOf(obj)
   const ctor = classFunction.prototype
   while(proto) {
