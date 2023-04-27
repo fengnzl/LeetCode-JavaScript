b3d8d9e8906904526c74b76c31fd0678e6a73d46
desireLearner <fengsongting@gmail.com>
2023-04-27 22:28:54 +0800

    Time: 68 ms (22.34%), Space: 42.2 MB (5.08%) - Leecode Sync Helper

---
+++ 2775-generate-fibonacci-sequence/2775-generate-fibonacci-sequence.js
@@ -0,0 +1,18 @@
+/**
+ * @return {Generator<number>}
+ */
+var fibGenerator = function*() {
+  let [a, b] = [0, 1]
+  yield a
+  yield b
+  while(true) {
+    [a, b] = [b, a + b]
+    yield b
+  }
+};
+
+/**
+ * const gen = fibGenerator();
+ * gen.next().value; // 0
+ * gen.next().value; // 1
+ */
