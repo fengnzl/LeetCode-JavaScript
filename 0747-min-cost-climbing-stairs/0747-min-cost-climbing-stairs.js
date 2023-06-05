e2764a000b512f9f1bc3025d84d91b8084d2ff4c
desireLearner <fengsongting@gmail.com>
2023-06-05 10:06:19 +0800

    Time: 68 ms (49.68%), Space: 41.8 MB (75.83%) - Leecode Sync Helper

---
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} cost
+ * @return {number}
+ */
+var minCostClimbingStairs = function(cost) {
+  const n = cost.length
+  const dp = Array(n).fill(0)
+  // 初始下标 0 开始爬
+  dp[0] = cost[0]
+  // 初始下标1 爬时最小值
+  dp[1] = cost[1]
+  for (let i = 2;i < n;i++) {
+    dp[i] = Math.min(dp[i - 1], dp[i - 2]) + cost[i]
+  }
+  return Math.min(dp[n - 1], dp[n - 2])
+};
