66aa5152bb31e9b6ecd2a3d2a220ee1fae48a91e
desireLearner <fengsongting@gmail.com>
2023-06-08 23:33:39 +0800

    Time: 56 ms (84.74%), Space: 41.1 MB (30.93%) - Leecode Sync Helper

--- 0198-house-robber/0198-house-robber.js
+++ 0198-house-robber/0198-house-robber.js
@@ -4,14 +4,11 @@
  */
 var rob = function(nums) {
   const n = nums.length
-  const dp = Array.from({ length: n }, () => Array(n).fill(0))
-  // dp[i][0] 代表第i 晚上不偷
-  // dp[i][1] 代表第 i 晚上偷
-  dp[0][0] = 0
-  dp[0][1] = nums[0]
-  for (let i = 1;i < n;i++) {
-    dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1])
-    dp[i][1] = dp[i - 1][0] + nums[i]
+  if (n === 1) return nums[0]
+  let first = nums[0]
+  let second = Math.max(nums[0], nums[1])
+  for (let i = 2;i < n;i++) {
+    [second, first] = [Math.max(first + nums[i], second), second]
   }
-  return Math.max(dp[n - 1][0], dp[n - 1][1])
+  return second
 };
