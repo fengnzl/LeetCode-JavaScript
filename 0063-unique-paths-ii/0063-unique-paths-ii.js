a25352f9ff10aadbb4fe79ec9cbe2d9b54df601a
desireLearner <fengsongting@gmail.com>
2023-06-13 09:58:50 +0800

    Time: 52 ms (98.64%), Space: 41.5 MB (71.34%) - Leecode Sync Helper

--- 0063-unique-paths-ii/0063-unique-paths-ii.js
+++ 0063-unique-paths-ii/0063-unique-paths-ii.js
@@ -3,31 +3,31 @@
  * @return {number}
  */
 var uniquePathsWithObstacles = function(obstacleGrid) {
-  const h = obstacleGrid.length
-  const w = obstacleGrid[0].length
-  const dp = Array.from({ length: h }, () => Array(w).fill(0))
-  for (let i = 0;i < h;i++) {
+  const m = obstacleGrid.length
+  const n = obstacleGrid[0].length
+  const dp = Array.from({ length: m }, () => Array(n).fill(0))
+  for (let i = 0;i < m;i++) {
     if (!obstacleGrid[i][0]) {
       dp[i][0] = 1
     } else {
       break
     }
   }
-  for (let i = 0;i < w;i++) {
+  for (let i = 0;i < n;i++) {
     if (!obstacleGrid[0][i]) {
       dp[0][i] = 1
     } else {
       break
     }
   }
-  for (let i = 1;i < h;i++) {
-    for (let j = 1;j < w;j++) {
-      if (obstacleGrid[i][j]) {
+  for (let i = 1;i < m;i++) {
+    for (let j = 1;j < n;j++) {
+      if (obstacleGrid[i][j] === 1) {
         dp[i][j] = 0
       } else {
-        dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+        dp[i][j] = dp[i - 1][j] + dp[i][j -1]
       }
     }
   }
-  return dp[h - 1][w - 1]
+  return dp[m - 1][n - 1]
 };
